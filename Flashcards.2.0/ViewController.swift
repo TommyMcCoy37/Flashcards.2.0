af33942dc9aeab40c5108dae5811c49c287d8ae6
Tommy McCoy <107891075+TommyMcCoy37@users.noreply.github.com>
2022-10-05 21:15:31 -0500

    Initial Commit

---
+++ Flashcards.2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards.2.0
+//
+//  Created by Tommy McCoy on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
